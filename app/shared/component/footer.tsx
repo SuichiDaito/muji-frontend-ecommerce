b0e14ae878aa9630b19388a1965b412c2e913ff2
suichi <suidaito969@gmail.com>
2026-05-28 13:52:53 +0700

    add | add header, footer component

---
+++ app/shared/component/footer.tsx
@@ -0,0 +1,11 @@
+import { defaultConfig } from "next/dist/server/config-shared";
+
+const Footer = () => {
+    return (
+        <div>
+            <p> this is footer for page</p>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ app/shared/component/header.tsx
@@ -0,0 +1,11 @@
+import { defaultConfig } from "next/dist/server/config-shared";
+
+const Header = () => {
+    return (
+        <div>
+            <p> this is header for page</p>
+        </div>
+    );
+}
+
+export default Header;
